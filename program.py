4191673b3ed43f3b04b633b01cc3eee727ad582f
tommasocalo <tommycalo96@gmail.com>
2020-12-16 18:05:37 +0100

    feature extractor

--- program.py
+++ program.py
@@ -1,4 +1,6 @@
 #/usr/bin/env python3
+from extractor import Extractor
+import numpy as np
 import cv2
 from display import Display
 w = 1920//2
@@ -6,9 +8,18 @@ h = 1080//2
 
 disp = Display(w,h)
 
+fe = Extractor()
+
 def process_frame(img):
    img = cv2.resize(img,(w,h))
+   matches = fe.extract(img)
+   for pt1,pt2 in matches:
+       u1, v1 = map(lambda x : int(round(x)), pt1)
+       u2, v2 = map(lambda x : int(round(x)), pt2)
+       cv2.circle(img, (u1,v1), color = (0,255,0) , radius=3)
+       cv2.line(img, (u1,v1),(u2,v2), color= (255,0,0))
    disp.draw(img)
+
 if __name__ == "__main__":
 
     cap = cv2.VideoCapture("videos/test_countryroad.mp4")
